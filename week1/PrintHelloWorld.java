55eb1ed8b76d74c16694ab4c41adaa00ec9a8c45
cypher-me <cyphermanu8@outlook.com>
2025-05-13 12:24:36 +0300

    added gitignore to git

---
+++ week1/PrintHelloWorld.java
@@ -0,0 +1,20 @@
+package week1;
+
+public class PrintHelloWorld {
+    public static void main(String args[]) {
+        System.out.println("manu");
+    }
+
+    void runSystemCheckToLookForMemorySize() {
+        long memorySize = Runtime.getRuntime().totalMemory();
+        System.out.println("Total Memory Size: " + memorySize + " bytes");
+
+    }
+
+
+    public void runCheckToCheckForTheSystemsOpenNetworkPorts() {
+        
+    }
+
+
+}
